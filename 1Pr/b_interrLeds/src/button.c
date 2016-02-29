9c3636d9d439effe698a8c3d6a9ef7e26412cabf
Usuario Local <Usuario Local>
2016-02-29 16:51:12 +0100

    no...

--- 1Pr/b_interrLeds/src/button.c
+++ 1Pr/b_interrLeds/src/button.c
@@ -24,30 +24,30 @@ void Eint4567_init(void)
 {
 /* Configuracion del controlador de interrupciones */
 	// Borra EXTINTPND escribiendo 1s en el propio registro
-	rEXTINTPND = 0xFF;
+	rEXTINTPND =  0xF;
 	// Borra INTPND escribiendo 1s en I_ISPC
-	rI_ISPC =  0x3FFFFFF;
+	rI_ISPC =  0xFFFFFFFF;
 	// Configura las lineas como de tipo IRQ mediante INTMOD
 	rINTMOD = 0x0000000;
 	// Habilita int. vectorizadas y la linea IRQ (FIQ no) mediante INTCON
 	rINTCON = 0x1;
 	// Enmascara todas las lineas excepto Eint4567 y el bit global (INTMSK)
 	// BIT_GLOBAL | BIT_EINT4567
-	rINTMSK = (~0x0) & (~ ( (1<<26) | (1<<21) ));
+	rINTMSK = 0xffffffff & ~BIT_GLOBAL & ~BIT_EINT4567;// ( (1<<26) | (1<<21) );
 	// Establecer la rutina de servicio para Eint4567
 	pISR_EINT4567 = (unsigned) Eint4567_ISR;
 
 /* Configuracion del puerto G */
 	// Establece la funcion de los pines (EINT7-EINT0)
-	rPCONG = rPCONG & (~0xC0); //6 y 7
+	rPCONG = 0xff3f; //6 y 7
 	// Habilita las resistencias de pull-up
 	rPUPG = 0x0;
 	// Configura las lineas de int. como de flanco de bajada mediante EXTINT
-	rEXTINT =  0x22000000;//(rEXTINT & 0x88ffffff) |
+	rEXTINT =  0x33333333;
 /* Por precaucion, se vuelven a borrar los bits de INTPND y EXTINTPND */
+	rEXTINTPND =  0xF;
+	rI_ISPC =  0xFFFFFFF;
 
-	rI_ISPC =  0x3FFFFFF;
-	rEXTINTPND =  0xFF;
 }
 
 /*COMENTAR PARA LA PARTE DEL 8-SEGMENTOS
@@ -58,12 +58,12 @@ void Eint4567_ISR(void)
 	//Conmutamos LEDs
 	leds_switch();
 	//Delay para eliminar rebotes
-	DelayMs(5000);
+	DelayMs(1000);
 	/*Atendemos interrupciones*/
 	//Borramos EXTINTPND ambas líneas EINT7 y EINT6
-	rEXTINTPND = 0xFF;
-	//Borramos INTPND usando ISPC
-	rI_ISPC =  0x3FFFFFF;
+	rEXTINTPND =  0xC;
+	rI_ISPC =  1<<21;
+
 }
 
 /*
--- 1Pr/b_interrLeds/src/main.c
+++ 1Pr/b_interrLeds/src/main.c
@@ -20,20 +20,24 @@ void Main(void)
 {
 	/* Inicializar controladores */
 	sys_init(); // Inicializacion de la placa, interrupciones y puertos
-	Eint4567_init();
+
 	//button_init();
 	/* Establecer valor inicial de los LEDs */
 	leds_off();
 	led1_on();
-	while (1);
-		/* Nada es por interrupcion */
+	Eint4567_init();
+	while (1){
+		//while(button_no_pressed() == 0);
+		//while(button_no_pressed() != 0);
 		//leds_switch();
+		//DelayMs(1000);
+	}
 
 }
 
 void button_init(){
 	/* Set 0 bits 6 and 7*/
-	rPCONG = rPCONG & (~0xC0);
+	rPCONG = rPCONG & (~0xF000);
 	/* rPUPG to 0's */
 	rPUPG = 0x0;
 
