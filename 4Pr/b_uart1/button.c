03db13c575eb27b0a03991935a8d680fb3f07019
Usuario Local <Usuario Local>
2016-04-08 17:02:45 +0200

    practica b

---
+++ 4Pr/b_uart1/button.c
@@ -0,0 +1,91 @@
+#include "44blib.h"
+#include "44b.h"
+#include "def.h"
+
+void Eint4567_ISR(void) __attribute__ ((interrupt ("IRQ")));
+void Eint4567_init(void);
+extern int button_no_pressed();
+
+extern luz;
+extern st;
+
+
+void Eint4567_init(void) {
+	/* Configuracion del controlador de interrupciones */
+		//Borrar interrupciones pendientes.
+		rI_ISPC = BIT_EINT4567;
+		rEXTINTPND = 0xf;
+
+		// Establecer la rutina de servicio para Eint4567
+	    pISR_EINT4567 = (unsigned)Eint4567_ISR;
+
+	/* Configuracion del puerto G */
+		// Establece la funcion de los pines (EINT7-EINT0)
+		rPCONG = rPCONG | (0xF000);
+
+		//Habilita las resistencias de pull-up
+		rPUPG = 0x0;
+
+		// Configura las lineas de int. como de flanco de bajada mediante EXTINT
+		rEXTINT = rEXTINT | 0x33000000;
+
+	/* Por precaucion, se vuelven a borrar los bits de INTPND y EXTINTPND */
+	    rEXTINTPND = 0xf;
+	    rI_ISPC = BIT_EINT4567;
+}
+
+
+void Eint4567_ISR(void)
+{
+	while(button_no_pressed() != 0);
+
+	int pulsado = rEXTINTPND & 0xC;
+
+	char c;
+	if(pulsado == 4){  //izq -> char I
+		Uart_SendByte('I');
+	} else if(pulsado == 8) { //dcha -> char D
+		Uart_SendByte('D');
+	}
+
+	//Delay para eliminar rebotes
+	DelayMs(100);
+
+	/*Atendemos interrupciones*/
+	//Borramos EXTINTPND ambas líneas EINT7 y EINT6
+	rEXTINTPND = 0xf; //((1<<2) | (1<<3));
+
+	//Borramos INTPND usando ISPC
+	rI_ISPC = BIT_EINT4567;
+}
+
+
+void Eint4567_activar(void){
+	//borrar interrupciones pendientes
+	rEXTINTPND = 0xf;
+	rI_ISPC = BIT_EINT4567;
+
+	//enmascarar la linea de interrupcion
+	rINTMSK = rINTMSK & ~( BIT_EINT4567 );
+
+	//borrar interrupciones pendientes
+	rEXTINTPND = 0xf;
+	rI_ISPC = BIT_EINT4567;
+}
+
+
+void Eint4567_desactivar(void){
+	//desenmarcarar la linea de interrupcion
+	rINTMSK = rINTMSK | BIT_EINT4567;
+
+	//borrar interrupciones pendientes
+	rEXTINTPND = 0xf;
+	rI_ISPC = BIT_EINT4567;
+}
+
+
+int button_no_pressed(){
+	UINT r = ~ ( rPDATG | ~ 0xC0 );
+	DelayMs(100);
+	return r;
+}
---
+++ 4Pr/b_uart1/main.c
@@ -0,0 +1,86 @@
+/*--- ficheros de cabecera ---*/
+#include "44blib.h"
+#include "44b.h"
+#include "uart.h"
+/*--- declaracion de funciones ---*/
+int Main(void);
+
+/*--- variables globales ---*/
+char str_send[10] = "\nSED-P4 >\0";
+char str_error[50] = "\nSe ha producido un desbordamiento!\n\0";
+char str[256];
+
+void initPractica();
+
+/*--- codigo de la funcion ---*/
+int Main(void){
+
+	char *pt_str = str;
+
+	sys_init(); // inicializacion de la placa, interrupciones, puertos
+
+	Uart_Init(115200); // inicializacion de la Uart
+	Uart_Config(); // configuración de interrupciones y buffers
+//	Uart_Printf(str_send); // mostrar cabecera
+
+	initPractica();
+	Eint4567_activar();
+
+
+	char c;
+	while(1){
+		c = Uart_Getch();
+		if(c == 'I'){
+			led1_off();
+			led2_on();
+		} else if(c=='D'){
+			led1_on();
+			led2_off();
+		} else {
+			led1_off();
+			led2_off();
+		}
+	};
+/*		*pt_str = Uart_Getch(); // leer caracter
+		Uart_SendByte(*pt_str); // enviar caracter
+		if (*pt_str == CR_char){ // retorno de carro?
+			if (pt_str != str) { // si cadena no vacia, mostrar
+				*(++pt_str) = '\0'; // terminar cadena antes
+				Uart_Printf("\n");
+				Uart_Printf(str);
+			}
+			Uart_Printf(str_send); // preparar siguiente
+			pt_str = str;
+		}
+		else if (++pt_str == str + 255){ // desbordamiento?
+			Uart_Printf(str_error); // avisar del desbordamiento
+			pt_str = str;
+		}
+	}
+*/
+}
+
+
+void initPractica(){
+	rI_ISPC = 0x3ffffff; //borrar interrupciones pendientes
+
+	// Configura las lineas como de tipo IRQ mediante INTMOD
+	rINTMOD = 0x0;
+	// Habilita int. vectorizadas y la linea IRQ (FIQ no) mediante INTCON
+	rINTCON = 0x1;
+
+	// Enmascara todas las lineas excepto el bit global (INTMSK)
+	rINTMSK = ~( BIT_GLOBAL );
+
+	//Iniciamos los distintos componentes
+	Eint4567_init();
+	keyboard_init();
+
+
+	//Apagamos todos los leds y 8 segmentos
+	leds_off();
+	D8Led_init();
+
+	/* Por precaucion, se vuelven a borrar los bits de INTPND y EXTINTPND */
+	rI_ISPC = 0x3ffffff;
+}
