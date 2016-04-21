1aec1f46542ec16ff068279c66aaa999006eea28
Usuario Local <Usuario Local>
2016-04-21 20:51:24 +0200

    arm project

---
+++ FINAL_PR/ARM/b_uart1/keyboard.c
@@ -0,0 +1,115 @@
+/*--- Ficheros de cabecera ---*/
+#include "44b.h"
+#include "44blib.h"
+#include "def.h"
+
+/*--- Definición de macros ---*/
+#define KEY_VALUE_MASK 0xF
+
+/*--- Variables globales ---*/
+volatile UCHAR *keyboard_base = (UCHAR *)0x06000000;
+int key;
+
+/*--- Funciones externas ---*/
+void D8Led_symbol(int value);
+
+/*--- Declaracion de funciones ---*/
+void keyboard_init();
+void Keyboard_ISR(void) __attribute__ ((interrupt ("IRQ")));
+
+
+
+/*--- Codigo de las funciones ---*/
+void keyboard_init()
+{
+	/* Configurar el puerto G (si no lo estuviese ya) */	
+		// Establece la funcion de los pines (EINT0-7)
+		rPCONG = rPCONG | 0xC;
+
+		// Habilita el "pull up" del puerto
+		rPUPG = 0x0;
+
+		// Configura las lineas de int. como de flanco de bajada mediante EXTINT
+		rEXTINT = rEXTINT | 0x00000030;
+
+		/* Establece la rutina de servicio para EINT1 */
+		pISR_EINT1 = (unsigned) Keyboard_ISR;
+
+		// Borra INTPND escribiendo 1s en I_ISPC
+		rI_ISPC = BIT_EINT1;
+
+}
+
+void keyboard_activar(){
+	//borrar interrupciones pendientes
+	rI_ISPC = BIT_EINT1;
+
+	// Habilitar linea EINT1
+	rINTMSK = rINTMSK & ~( BIT_EINT1 );
+
+	//borrar interrupciones pendientes
+	rI_ISPC = BIT_EINT1;
+}
+
+void keyboard_desactivar(){
+	// Desabilitar linea EINT1
+	rINTMSK = rINTMSK | BIT_EINT1;
+
+	//borrar interrupciones pendientes
+	rI_ISPC = BIT_EINT1;
+}
+
+
+void Keyboard_ISR(void)
+{
+	/* Esperar trp mediante la funcion DelayMs()*/
+	DelayMs(20);//trp=20ms
+
+	/* Identificar la tecla */
+	int pulsado = key_read();
+	Uart_SendByte1(pulsado);
+		if (pulsado > 9){
+			pulsado = pulsado - 10;
+
+			Uart_SendByte0(pulsado +'A');
+
+		}else{
+			//Uart_SendByte1(pulsado +'0');
+			Uart_SendByte0(pulsado +'0');
+		}
+
+	/* Esperar a se libere la tecla: consultar bit 1 del registro de datos del puerto G */
+	while (!(rPDATG & 0x2)){}
+
+	/* Esperar trd mediante la funcion Delay() */
+	DelayMs(100);//trd=100ms
+	/* Borrar interrupción de teclado */
+	rI_ISPC = BIT_EINT1;
+}
+
+int key_read()
+{
+	short int i=0;
+
+	int value= -1;
+	char temp;
+	// Identificar la tecla mediante ''scanning''
+	// Para el juego solo es necesario detectar las dos primeras teclas.
+
+	unsigned int offset[4] = {0xFD,0xFB,0xF7,0xEF};
+
+	for(i=0; i<4; i++){
+		temp = *(keyboard_base + (offset[i])) & KEY_VALUE_MASK;
+
+		switch (temp) {
+			case 0x7:  value = 0 + (i*4); break;
+			case 0xB:  value = 1 + (i*4); break;
+			case 0xD:  value = 2 + (i*4); break;
+			case 0xE:  value = 3 + (i*4); break;
+		}
+	}
+
+	return value;
+
+
+}
---
+++ FINAL_PR/ARM/b_uart1/main.c
@@ -0,0 +1,114 @@
+/*--- ficheros de cabecera ---*/
+#include "44blib.h"
+#include "44b.h"
+#include "uart.h"
+/*--- declaracion de funciones ---*/
+int Main(void);
+
+/*--- variables globales ---*/
+char str_send[10] = "\nSED-P4 >\0";
+char str_error[50] = "\nSe ha producido un desbordamiento!\n\0";
+char str[256];
+
+void initPractica();
+
+/*--- codigo de la funcion ---*/
+int Main(void){
+
+	char *pt_str = str;
+
+	sys_init(); // inicializacion de la placa, interrupciones, puertos
+
+	Uart0_Init(115200); // inicializacion de la Uart
+	Uart1_Init(19200); // inicializacion de la Uart
+	Uart_Config(); // configuración de interrupciones y buffers
+//	Uart_Printf(str_send); // mostrar cabecera
+
+	initPractica();
+	Eint4567_activar();
+	keyboard_activar();
+
+	char c;
+	while(1){
+		c = Uart_Getch1();
+		Uart_SendByte0(c);
+		switch(c){
+		case 'L':
+			led1_on();
+			led2_off();
+			break;
+		case 'R':
+			led1_off();
+			led2_on();
+			break;
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':
+			D8Led_symbol(c-'0');
+			led1_off();
+			led2_off();
+			break;
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+		case 'F':
+			D8Led_symbol(c-'A'+10);
+			led1_off();
+			led2_off();
+			break;
+
+		}
+
+	};
+/*		*pt_str = Uart_Getch(); // leer caracter
+		Uart_SendByte(*pt_str); // enviar caracter
+		if (*pt_str == CR_char){ // retorno de carro?
+			if (pt_str != str) { // si cadena no vacia, mostrar
+				*(++pt_str) = '\0'; // terminar cadena antes
+				Uart_Printf("\n");
+				Uart_Printf(str);
+			}
+			Uart_Printf(str_send); // preparar siguiente
+			pt_str = str;
+		}
+		else if (++pt_str == str + 255){ // desbordamiento?
+			Uart_Printf(str_error); // avisar del desbordamiento
+			pt_str = str;
+		}
+	}
+*/
+}
+
+
+void initPractica(){
+	rI_ISPC = 0x3ffffff; //borrar interrupciones pendientes
+
+	// Configura las lineas como de tipo IRQ mediante INTMOD
+	rINTMOD = 0x0;
+	// Habilita int. vectorizadas y la linea IRQ (FIQ no) mediante INTCON
+	rINTCON = 0x1;
+
+	// Enmascara todas las lineas excepto el bit global (INTMSK)
+	rINTMSK = ~( BIT_GLOBAL );
+
+	//Iniciamos los distintos componentes
+	Eint4567_init();
+	keyboard_init();
+
+
+	//Apagamos todos los leds y 8 segmentos
+	leds_off();
+	D8Led_init();
+
+	/* Por precaucion, se vuelven a borrar los bits de INTPND y EXTINTPND */
+	rI_ISPC = 0x3ffffff;
+}
